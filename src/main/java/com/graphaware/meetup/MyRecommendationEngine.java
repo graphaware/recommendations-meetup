f80b9cd5fe81350e1c0f3247eff34c56947dfece
Michal Bachman <michal@graphaware.com>
2015-03-26 23:31:23 +0000

    upgrade to latest recommendation engine version

--- src/main/java/com/graphaware/meetup/MyRecommendationEngine.java
+++ src/main/java/com/graphaware/meetup/MyRecommendationEngine.java
@@ -2,6 +2,8 @@ package com.graphaware.meetup;
 
 import com.graphaware.meetup.engine.DummyEngine;
 import com.graphaware.reco.generic.engine.RecommendationEngine;
+import com.graphaware.reco.generic.filter.BlacklistBuilder;
+import com.graphaware.reco.generic.filter.Filter;
 import com.graphaware.reco.generic.log.Logger;
 import com.graphaware.reco.generic.post.PostProcessor;
 import com.graphaware.reco.neo4j.engine.Neo4jTopLevelDelegatingEngine;
@@ -13,10 +15,6 @@ import java.util.List;
 
 public class MyRecommendationEngine extends Neo4jTopLevelDelegatingEngine {
 
-    public MyRecommendationEngine() {
-        super(new MyContextFactory());
-    }
-
     @Override
     protected List<RecommendationEngine<Node, Node>> engines() {
         return Arrays.<RecommendationEngine<Node, Node>>asList(
@@ -27,6 +25,16 @@ public class MyRecommendationEngine extends Neo4jTopLevelDelegatingEngine {
     @Override
     protected List<PostProcessor<Node, Node>> postProcessors() {
         return Collections.emptyList(); //todo add your own post processors
+    }
+
+    @Override
+    protected List<BlacklistBuilder<Node, Node>> blacklistBuilders() {
+        return Collections.emptyList(); //todo add your own blacklist builders instead
+    }
+
+    @Override
+    protected List<Filter<Node, Node>> filters() {
+        return Collections.emptyList(); //todo add your own filters instead
     }
 
     @Override
